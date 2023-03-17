3f21ae33d3ba8b995f10f7649da15a8f86dc9e1b
Audrey <audreyjloisy@hotmail.com>
2023-03-16 20:45:37 -0400

    navigation function

--- app/src/main/java/com/example/prototype/MainActivity.java
+++ app/src/main/java/com/example/prototype/MainActivity.java
@@ -1,16 +1,39 @@
 package com.example.prototype;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        //Hooks//
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar_home);
+
+        //toolbar//
+
+        setSupportActionBar(toolbar);
+
+
+        //navigation menu//
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle()
 
     }
 }
